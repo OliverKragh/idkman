c530c0e1bf99ecdd5c280372013fa6a4f23b86f4
OliverKragh <98881951+OliverKragh@users.noreply.github.com>
2022-02-02 19:28:13 +0100

    t

--- bob.py
+++ bob.py
@@ -1,8 +1,8 @@
 from traceback import format_stack
 from urllib.request import urlopen
+from winreg import HKEY_LOCAL_MACHINE
 
 
 
 
 urlopen("https://www.learnpython.org/en/Basic_String_Operations")
-
